89443b3b5d7e1a09611884c2a8f9d777e190a449
0x6a69616e <0x6a69616e@gmail.com>
2024-03-07 17:07:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const
-    axios = require('axios'),
-    cheerio = require('cheerio');
+  axios = require('axios'),
+  cheerio = require('cheerio');
 
 async function getTranscript(id) {
-    const
-      response = await axios.get(`https://youtubetranscript.com/?server_vid2=${id}`),
-      $ = cheerio.load(response.data, undefined, false),
-      err = $('error');
+  const url = new URL('https://youtubetranscript.com');
+  url.searchParams.set('server_vid2', id);
   
-    if (err.length) throw new Error(err.text());
-    return $('transcript text').map((i, elem) => {
-        const $a = $(elem);
-        return {
-            text: $a.text(),
-            start: $a.attr('start'),
-            duration: $a.attr('dur')
-        };
-    }).toArray();
+  const
+    response = await axios.get(url),
+    $ = cheerio.load(response.data, undefined, false),
+    err = $('error');
+
+  if (err.length) throw new Error(err.text());
+  return $('transcript text').map((i, elem) => {
+    const $a = $(elem);
+    return {
+      text: $a.text(),
+      start: Number($a.attr('start')),
+      duration: Number($a.attr('dur'))
+    };
+  }).toArray();
 }
 
 async function validateID(id) {
-    try {
-        await axios.get(`https://video.google.com/timedtext?type=track&v=${id}&id=0&lang=en`);
-        return !0;
-    } catch (_) {
-        return !1;
-    }
+  const url = new URL('https://video.google.com/timedtext');
+  url.searchParams.set('type', 'track');
+  url.searchParams.set('v', id);
+  url.searchParams.set('id', 0);
+  url.searchParams.set('lang', 'en');
+  
+  try {
+    await axios.get(url);
+    return !0;
+  } catch (_) {
+    return !1;
+  }
 }
 
 module.exports = { getTranscript, validateID };
